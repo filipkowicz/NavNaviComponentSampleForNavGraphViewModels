ac617ebd1052d3dae088364b1258675a48de47a3
Michal Filipek <filipkowicz@gmail.com>
2022-02-18 16:27:31 +0100

    SavedStateHandle in VM

--- app/src/main/java/com/filipkowicz/navcomponentsample/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/filipkowicz/navcomponentsample/ui/dashboard/DashboardViewModel.kt
@@ -2,12 +2,19 @@ package com.filipkowicz.navcomponentsample.ui.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class DashboardViewModel(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is dashboard Fragment" + savedStateHandle.get(ARG)
     }
     val text: LiveData<String> = _text
+
+    companion object {
+        const val ARG = "dashboard_arg"
+    }
 }
